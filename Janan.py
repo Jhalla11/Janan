d2b3bd30a77e49c6ca9e6c876fd55ca0768ab0aa
Jhalla11 <116496651+Jhalla11@users.noreply.github.com>
2023-01-16 14:34:05 +0500

    Update Janan.py
    git clone https://github.com/Jhalla11/Janan.git
    ls
    cd Janan
    ls
    python Janan.py

--- Janan.py
+++ Janan.py
@@ -183,7 +183,7 @@ def rcrack(uid,pwx,tl):
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-             'user-agent':'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36',}
+             'user-agent':'Mozilla/5.0 (Linux; Android 12; 22101316C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/108.0.5359.128 Mobile Safari/537.36',}
             lo = session.get('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
